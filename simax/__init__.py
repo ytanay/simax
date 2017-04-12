2d59c4cba9007c06d030eb70a4d9f10b17643bec
Yotam Tanay <yotam@paticon.com>
2017-04-12 09:15:33 +0300

    Removed old CLI code from original entry point

--- simax/__init__.py
+++ simax/__init__.py
@@ -1,26 +1,3 @@
-import random
-import time
+from simax.interface.interface import Interface
 
-from entitiy.park import Park
-from person.person import Person
-from world.world import World
-
-world = World()
-import os
-
-clear = lambda: os.system('cls')
-
-for _ in range(5):
-    world.assign(Person(*world.random_placement()))
-
-for _ in range(3):
-    world.assign(Park(*world.random_placement()))
-
-
-while True:
-
-    world.draw_and_cycle()
-
-    #input()
-    time.sleep(0.2)
-    clear()
+Interface().start()
